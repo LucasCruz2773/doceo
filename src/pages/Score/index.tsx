918953bb93d1ae4bcc53676016fe50e273af2aca
Lucas Cruz <lucascruz2773@gmail.com>
2020-11-30 22:04:51 -0300

    Criação da página de avaliação dos professores

---
+++ src/pages/Score/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+import PageHeader from '../../components/PageHeader';
+
+import starImg from '../../assets/images/star.png';
+import goldStarImg from '../../assets/images/gold-star.png';
+
+import './styles.css';
+
+
+function Score(){
+    
+    
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    return(
+        <div id="page-score" className="container">
+            <PageHeader title="Avalie sua aula" description="Como foi a aula com Fulano?">
+                <div id="score">
+                    
+                    <img src={goldStarImg} width='20' height='20' alt="Proffy"/>
+                    <img src={goldStarImg} width='20' height='20' alt="Proffy"/>
+                    <img src={goldStarImg} width='20' height='20' alt="Proffy"/>
+                    <img src={goldStarImg} width='20' height='20' alt="Proffy"/>
+                    <img src={starImg} width='20' height='20' alt="Proffy"/>
+
+                    <button type="submit">
+                        Avaliar
+                    </button>
+                </div>
+                
+            </PageHeader>
+            <main>
+                <div id="content">
+                    Obrigado por utilizar nossa plataforma!
+                </div>
+            </main>
+
+        </div>
+    )
+}
+
+export default Score;
